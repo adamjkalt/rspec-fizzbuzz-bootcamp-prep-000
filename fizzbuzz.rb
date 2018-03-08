32d1e8c7e16bb362d5e883b14860f3394e6c84c4
Adam Kalt <adam.kalt.1@gmail.com>
2018-03-08 04:48:43 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,10 +1,10 @@
 def fizzbuzz(int)
-  if int % 3 == 0
-    "Fizz"
-  elseif int % 5 == 0
-    "Buzz"
-  elseif (int % 3 == 0 && int % 5 == 0)
-    "FizzBuzz"
-  else (int % 3 !== 0 || int % 5 !== 0)
-    nil
+  if (int % 3 == 0 && int % 5 == 0)
+    puts "FizzBuzz"
+  elsif int % 3 == 0
+    puts "Fizz"
+  elsif int % 5 == 0
+    puts "Buzz"
+  else
+    puts nil
 end
